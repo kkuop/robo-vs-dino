3273fb9826460d918f0c5b795fd675cc7167e01e
kkuop <60794860+kkuop@users.noreply.github.com>
2020-02-10 08:29:40 -0600

    realized that the attack power levels were too similar and the fight was literally ending with both sides killing each other... changed the attack power levels with more difference and now the ending makes more sense to the human eye... could also optimize what happens when the power levels/energy run out

--- RobotsVSDinosaurs/Battlefield.cs
+++ RobotsVSDinosaurs/Battlefield.cs
@@ -12,11 +12,11 @@ namespace RobotsVSDinosaurs
         public void StartBattle()
         {
             //instantiate the 3 dinos and 3 robots and add them to a list
-            Dinosaur raptor = new Dinosaur("raptor", 100, 100, 15);
-            Dinosaur bronto = new Dinosaur("brontosaurus", 100, 80, 5);
-            Dinosaur stego = new Dinosaur("stegosaurus", 100, 75, 8);
-            Robot blueRobot = new Robot("caboose", 100, 100, new Weapon("Energy Sword", 15));
-            Robot redRobot = new Robot("Sarge", 100, 100, new Weapon("Assault Rifle", 5));
+            Dinosaur raptor = new Dinosaur("raptor", 100, 100, 8);
+            Dinosaur bronto = new Dinosaur("brontosaurus", 100, 80, 2);
+            Dinosaur stego = new Dinosaur("stegosaurus", 100, 75, 6);
+            Robot blueRobot = new Robot("caboose", 100, 100, new Weapon("Energy Sword", 22));
+            Robot redRobot = new Robot("Sarge", 100, 100, new Weapon("Assault Rifle", 12));
             Robot pinkRobot = new Robot("Simmons", 100, 100, new Weapon("Sniper", 25));
             Herd dinos = new Herd(new List<Dinosaur> { raptor, bronto, stego });
             Fleet robots = new Fleet(new List<Robot> { blueRobot, redRobot, pinkRobot });
@@ -25,29 +25,46 @@ namespace RobotsVSDinosaurs
             //set the variables for the attackCounter and how many attacks each warrior gets
             int attackCounter = 0;
             int attacksPerFighter = 1;
-            while ((dinos.listOfDinos[0].isAlive != false || dinos.listOfDinos[1].isAlive != false || dinos.listOfDinos[2].isAlive != false) || (robots.listOfRobots[0].isAlive != false || robots.listOfRobots[1].isAlive != false || robots.listOfRobots[2].isAlive != false))
+            bool dinosAreDead = false;
+            bool robotsAreDead = false;
+            int costOfAttack = 10;
+            //while loop that runs while both sides are still alive
+            while (dinosAreDead == false && robotsAreDead == false)
             {
-                for (int i=0; i<dinos.listOfDinos.Count;i++)
-            {
-                while (attackCounter<attacksPerFighter)
+                if (dinos.listOfDinos[0].isAlive == false && dinos.listOfDinos[1].isAlive == false && dinos.listOfDinos[2].isAlive == false)
                 {
+                    dinosAreDead = true;
+                    continue;
+                }
+                if (robots.listOfRobots[0].isAlive == false && robots.listOfRobots[1].isAlive == false && robots.listOfRobots[2].isAlive == false)
+                {
+                    robotsAreDead = true;
+                    continue;
+                }
+                for (int i=0; i<dinos.listOfDinos.Count;i++)
+                {
+                    while (attackCounter<attacksPerFighter)
+                    {
                         if (dinos.listOfDinos[i].health > 0)
                         {
                             dinos.listOfDinos[i].health -= robots.listOfRobots[i].weapon.attackPower;
+                            dinos.listOfDinos[i].energy -= costOfAttack;
                             attackCounter++;
                         }
                         else
                         {
                             dinos.listOfDinos[i].isAlive = false;
+                            dinos.listOfDinos[i].health = 0;
                             break;
+                            }
                         }
-                }
                     attackCounter = 0;
-                    if (dinos.listOfDinos[i].health<0)
+                    if (dinos.listOfDinos[i].health<=0)
                     {
                         dinos.listOfDinos[i].isAlive = false;
+                        dinos.listOfDinos[i].health = 0;
                     }
-            }
+                }
             for (int i=0;i<robots.listOfRobots.Count;i++)
             {
                 while (attackCounter<attacksPerFighter)
@@ -55,6 +72,7 @@ namespace RobotsVSDinosaurs
                         if (robots.listOfRobots[i].health > 0)
                         {
                             robots.listOfRobots[i].health-=dinos.listOfDinos[i].attackPower;
+                            robots.listOfRobots[i].powerLevel -= costOfAttack;
                             attackCounter++;
                         }
                         else
@@ -64,9 +82,9 @@ namespace RobotsVSDinosaurs
                         }
                 }
                     attackCounter = 0;
-                    if (robots.listOfRobots[i].health<0)
+                    if (robots.listOfRobots[i].health<=0)
                     {
-                        robots.listOfRobots[i].isAlive = false;
+                        robots.listOfRobots[i].isAlive = false;                        
                     }
             }
             }
